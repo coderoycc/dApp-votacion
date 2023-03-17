7672e8cae6a47eb491dcf189afd96f3c2333d599
coderoycc <rcchambi4@umsa.bo>
2023-03-16 22:05:05 -0400

    Test totalSupply - direcciones Votacion

--- contracts/Votacion.sol
+++ contracts/Votacion.sol
@@ -6,17 +6,15 @@ contract Votacion is ERC20Basic{
     // string title;
 
     //votantes direcciones
-    address [] votantes;
+    address [10] public votantes;
     
     // candidatos direcciones Cuatro candidatos
     address[4] public candidatos;
 
     constructor() ERC20Basic(10){
         owner = msg.sender;
-        candidatos[0] = address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
-        candidatos[1] = address(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
-        candidatos[2] = address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
-        candidatos[3] = address(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
+        recibirCandidatos();
+        recibirVotantes();
     }
 
     function verCandidatos() public view returns(address [4] memory){
@@ -43,4 +41,21 @@ contract Votacion is ERC20Basic{
         return valanceOf(_candidato);
     }
 
+    function recibirCandidatos() private {
+        candidatos[0] = address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        candidatos[1] = address(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        candidatos[2] = address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+        candidatos[3] = address(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
+    }
+    function recibirVotantes() private {
+        votantes[0] = address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        votantes[1] = address(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        votantes[2] = address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+        votantes[3] = address(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
+        votantes[4] = address(0xFf027902FE23dD09f4C38FC534aE9e107eEe7E42);
+        votantes[5] = address(0xFA4d70462809B43F529FFb1DE9C077D094e6ab8a);
+        votantes[6] = address(0x2bcD78720142835143819AF9b145A51F967eFbB6);
+        votantes[7] = address(0xEF10f0930298674FeD4cD2d0CFcFe9D398Da1fe6);
+        votantes[8] = address(0x80dC5fC8b93E46A53d0340DF43F16AE9Ff4FfCED);
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,22 +1,12 @@
-// Right click on the script name and hit "Run" to execute
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+var Votacion = artifacts.require('./Votacion.sol');
 
-describe("Votación", function () {
-    it("Prueba de Equilibrio de Saldo", async function () {
-        const Votacion = await ethers.getContractFactory("contracts/ERC20.sol:ERC20Basic");
-        const votacion = await Votacion.deploy("FMK",10);
-        await votacion.deployed();
-        console.log('Desplegado con la dirección: '+ votacion.address)
-        expect((await votacion.totalSupply()).toNumber()).to.equal(10);
-    });
-    /*it("test updating and retrieving updated value", async function () {
-        const Storage = await ethers.getContractFactory("Storage");
-        const storage = await Storage.deploy();
-        await storage.deployed();
-        const storage2 = await ethers.getContractAt("Storage", storage.address);
-        const setValue = await storage2.store(56);
-        await setValue.wait();
-        expect((await storage2.retrieve()).toNumber()).to.equal(56);
-    });*/
-});
+contract("Votacion", (cuentas) => {
+  var app;
+  
+  it("TotalSupply", async ()=>{
+    app = await Votacion.deployed();
+    const total = await app.totalSupply();
+    const totalNum = parseInt(total.toString());
+    assert.equal(totalNum, 10);
+  })
+})
